0769789863d9ba016651ad856c4f6cfd85f80712
moyarzun <moyarzun@buk.cl>
2024-09-06 16:11:40 -0400

    feat: archivos base para Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PostList from './components/PostList';
+import AddPost from './components/AddPost';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddPost />
+      <PostList />
     </div>
   );
-}
+};
 
 export default App;
